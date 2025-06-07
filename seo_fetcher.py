b230f586afb7206cd7f843df1a79478ee0b8c9d9
Bipin Gowda <bipin.919@gmail.com>
2025-06-07 08:01:51 +0530

    Removed comments

--- seo_fetcher.py
+++ seo_fetcher.py
@@ -1,6 +1,4 @@
-# Mock SEO Data Fetcher
 def fetch_seo_data(keyword):
-    # In a real-world case, integrate with Ahrefs/SEMrush API
     return {
         "search_volume": 12000,
         "keyword_difficulty": 35,
